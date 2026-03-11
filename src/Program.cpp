c7faa8b3683698d1a1f093f944c70aab0ed6f073
damian.fiol@upr.edu <damian.fiol@upr.edu>
2026-03-11 16:18:45 -0400

    added name

--- src/Program.cpp
+++ src/Program.cpp
@@ -17,8 +17,8 @@ Program::Program() {
         });
 
     for (int i = 0; i < 30; i++) {
-        float x = 250 + 50 * i;
-        float y = 200 + 50 * i;
+        float x = 250 + 50 * (i%10);
+        float y = 200 + 50 * (i/10);
 
         Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
             std::pair<float, float>{x, y}, 
@@ -58,6 +58,9 @@ void Program::Update() {
         for (Projectile& p : Projectile::projectiles) { 
             p.update(); 
 
+            if (p.ID !=0 && HitBox::Collision(player -> hitBox, p.hitBox)){
+                PlayerReset();
+            }
         }
 
         if (lives <= 0 && pauseFrames <= 0) gameOver = true;
